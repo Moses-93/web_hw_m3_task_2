10d9dd275dc4b0f2b0edc3daad417d6fb7a4109b
moses93 <moieseenko2001@gmail.com>
2024-07-27 11:56:54 +0300

    task completed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # web_hw_m3_task_2
+
+Clone this repository:
+
+```bash
+git clone https://github.com/Moses-93/web_hw_m3.git
+```
---
+++ main.py
@@ -0,0 +1,44 @@
+from multiprocessing import Pool
+import time
+
+def factorize(number):
+    """
+    This A function for calculating the factors of a single number.
+
+    Parameters:
+    number (int): The number for which to calculate the factors.
+
+    Returns:
+    tuple: A tuple containing the original number and a sorted list of its factors.
+    """
+    factors = []
+    for i in range(1, int(number**0.5) + 1):
+        if number % i == 0:
+            factors.append(i)
+            if i != number // i:
+                factors.append(number // i)
+    return number, sorted(factors)
+
+start_time = time.time()
+def processing(*numbers):
+    """
+    This function processes a list of numbers by calculating their factors using multiprocessing.
+
+    Parameters:
+    *numbers (int): Variable length argument list of numbers to process.
+
+    Returns:
+    None
+    """
+    start_time = time.time()
+    with Pool() as pool:
+        results = pool.map(factorize, numbers)
+    end_time = time.time()
+    for number, factors in results:
+        print(f"The factors of {number} are: {factors}")
+    print(f"Time taken: {end_time - start_time:.4f} seconds")
+
+    
+if __name__ == '__main__':
+    numbers = [128, 255, 99999, 10651060]
+    processing(*numbers)
